21a56497aad0e0da41465797ec60f64020a12f2d
faith musa <faithy@faiths-MacBook-Air.local>
2022-03-26 01:44:30 -0500

    extra comments

--- bonus.js
+++ bonus.js
@@ -11,11 +11,14 @@ const array_one = [
 ];
 
 const array_two = ["dog", "goose", "kite", "meal", "laptop"];
-
+// I created an empty array, that's going to be populated with matching values.
 const matchValues = [];
+//takes each value of array_one and compares it to array_two
 array_one.map((x) => {
+
   for (let i = 0; i < array_two.length; i++) {
     if (x === array_two[i]) {
+      //pushes the matching value on to matchValues
       return matchValues.push(x);
     }
   }
